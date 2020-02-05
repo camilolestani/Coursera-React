a8b8ae18377c15909ef8e99f10e8cdc6514bc424
Camilo <Lestani>
2020-02-05 11:12:05 -0300

    Week 1 Assignament final

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -12,22 +12,39 @@ class DishDetail extends Component {
   constructor(props) {
     super(props);
     this.renderDish = this.renderDish.bind(this);
-
-    this.state = {};
   }
 
-  renderDish() {
-    var dish = this.props.selectedDish;
-    if (dish != null) {
+  renderDish(dish) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <Card>
+          <CardImg width="100%" object src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText> {dish.description} </CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  renderComments(comments) {
+    if (comments != null) {
+      const dishComments = comments.map(comment => {
+        return (
+          <div>
+            <li className="mt-3 mb-3">{comment.comment}</li>
+            <li className="mt-3 mb-3">
+              -- {comment.author} , {comment.date}{" "}
+            </li>
+          </div>
+        );
+      });
+
       return (
         <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" object src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText> {dish.description} </CardText>
-            </CardBody>
-          </Card>
+          <h4>Comments</h4>
+          <ul className="list-unstyled">{dishComments}</ul>
         </div>
       );
     } else {
@@ -35,40 +52,17 @@ class DishDetail extends Component {
     }
   }
 
-  renderComments() {
-    var dish = this.props.selectedDish;
-    if (dish != null) {
-      if (dish.comments != null) {
-        console.log(dish.comments);
-        const comments = dish.comments.map(comment => {
-          return (
-            <div>
-              <li className="mt-3 mb-3">{comment.comment}</li>
-              <li className="mt-3 mb-3">
-                -- {comment.author} , {comment.date}{" "}
-              </li>
-            </div>
-          );
-        });
-        return (
-          <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            <ul className="list-unstyled">{comments}</ul>
-          </div>
-        );
-      }
+  render() {
+    if (this.props.selectedDish != null) {
+      return (
+        <div className="row">
+          {this.renderDish(this.props.selectedDish)}
+          {this.renderComments(this.props.selectedDish.comments)}
+        </div>
+      );
     } else {
       return <div></div>;
     }
-  }
-
-  render() {
-    return (
-      <div className="row">
-        {this.renderDish()}
-        {this.renderComments()}
-      </div>
-    );
   }
 }
 
